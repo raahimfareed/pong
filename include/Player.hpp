3e4d5c61057a22ea0f2baa297692ccb2bbf4076b
Raahim Fareed <raahim1965@hotmail.com>
2021-08-04 22:55:05 +0500

    Added member functions to move the player up and down

--- include/Player.hpp
+++ include/Player.hpp
@@ -7,4 +7,12 @@ class Player: public Entity
 public:
     Player();
     Player(SDL_Texture* pTexture, SDL_Rect currentFrame, Vector2f position = Vector2f(), Vector2f dimensions = Vector2f(16, 16));
+
+    void up();
+    void down();
+    void stop();
+    void update();
+
+private:
+    float mVelocity;
 };
--- src/player.cpp
+++ src/player.cpp
@@ -1,7 +1,31 @@
 #include "Player.hpp"
+#include "Config.hpp"
 
 Player::Player() = default;
 
 Player::Player(SDL_Texture* pTexture, SDL_Rect currentFrame, Vector2f position, Vector2f dimensions)
-    : Entity(pTexture, currentFrame, position, dimensions)
+    : Entity(pTexture, currentFrame, position, dimensions), mVelocity(0.0f)
 {}
+
+void Player::up()
+{
+    mVelocity = -5.f;
+}
+
+void Player::down()
+{
+    mVelocity = 5.f;
+}
+
+void Player::stop()
+{
+    mVelocity = 0;
+}
+
+void Player::update()
+{
+    if (!(mPosition.mY + mVelocity > Config::screenHeight - mDimensions.mY) && mPosition.mY + mVelocity >= 0)
+    {
+        mPosition.mY += mVelocity;
+    }
+}
